7757e428d17ccf4046dfaf7ce9cadc9a9ae0a57f
swapnil singh <swappp7singh@gmail.com>
2018-07-26 21:42:39 +0530

    Included the opencv library and loaded the face and eye classifiers

---
+++ Module_1_Face_Recognition/face_recognition.py
@@ -0,0 +1,9 @@
+# Face Recognition
+
+# Importing the libraries
+import cv2
+
+# Loading the cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
